29577b17f0199dae7ff2d01452effda79f7452fe
Jay Heng <jie.heng@nxp.com>
2020-01-06 15:15:45 +0800

    [RT600 A0][JLink] Initial project based on rom api
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

---
+++ boards/evkmimxrt685/flash_algo_a0_silicon/Keil_JLink/RTE/_MIMXRT6XX_EVK_QSPI/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'MIMXRT6XX_EVK_QSPI' 
+ * Target:  'MIMXRT6XX_EVK_QSPI' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM33_DSP_FP_TZ.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
