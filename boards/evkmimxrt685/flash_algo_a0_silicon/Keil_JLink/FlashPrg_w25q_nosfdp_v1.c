dd129c56dfabf156207a4b3b1aa52ea4dec41741
Jay Heng <jie.heng@nxp.com>
2020-01-06 16:24:50 +0800

    [RT600 A0][JLink][MSFT] V1.1 version from Bryan Cole
    - Manually create w25q_config variable and set it as member of flashConfig
    - Don't need to call quadspi_nor_get_config() any more, as we have initialized flashConfig
    
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- boards/evkmimxrt685/flash_algo_a0_silicon/Keil_JLink/FlashPrg_w25q_nosfdp_v1.c
+++ boards/evkmimxrt685/flash_algo_a0_silicon/Keil_JLink/FlashPrg_w25q_nosfdp_v1.c
@@ -38,9 +38,82 @@
 /** local definitions **/
 
 #define FLASH_BASE_ADDR 0x08000000
-#define CONFIG_OPTION 0xc0000000
 
 quadspi_nor_config_t flashConfig = {.pageSize = 0x400};
+
+quadspi_mem_config_t w25q_config = {
+  .tag = QUADSPI_CFG_BLK_TAG,
+  .version = QUADSPI_CFG_BLK_VERSION,
+  .readSamplingOption = 0x3,
+  .csHoldTime = 0x3,
+  .csSetupTime = 0x3,
+  .columnAddressWidth = 0x0,
+  .deviceModeCfgEnable = 0x0,
+  .deviceModeType = 0x0,
+  .waitTimeCfgCommands = 0x0,
+  .deviceModeSeq = 0x0,
+  .deviceModeArg = 0x0,
+  .configCmdEnable = 0x0,
+  .configModeType = 0x0,
+  .configCmdSeqs = 0x0,
+  .configCmdArgs = 0x0,
+  .controllerMiscOption = 0x0,
+  .deviceType = 0,
+  .sflashPadType = kSerialFlash_1Pad,
+  .serialClkFreq = kQuadSpiSerialClk_50MHz,
+  .sflashSize[0] = 0x200000,
+  .sflashSize[1] = 0,
+  .sflashSize[2] = 0,
+  .sflashSize[3] = 0,	
+  .csPadSettingOverride = 0,
+  .sclkPadSettingOverride = 0,
+  .dataPadSettingOverride = 0,
+  .dqsPadSettingOverride = 0,
+  .timeoutInMs = 0,
+  .coarseTuning = 0,
+  .fineTuning = 0x0,
+  .samplePoint = 0x0,
+  .dataHoldTime = 0x0,
+  .busyOffset = 0,
+  .busyBitPolarity = 0,
+  .lookupTable = {
+  /* Seq 0 :Single Read */
+  /* CMD:        0x03 - Single Read, Single pad */
+  /* ADDR:       0x18 - 24bit address, Single pads */
+  /* READ:       0x80 - Read 128 bytes, Single pads */		
+  [0] = 0x08180403, [1] = 0x24001C80, 
+
+  /* Seq 1: Read Status Register 1*/
+  /* CMD:    0x05 - Read Status, single pad */
+  /* READ:   0x01 - Read 1 byte */
+  [4] = 0x1C040405, 
+    
+  /* Seq 3: Write Enable */
+  /* CMD:      0x06 - Write Enable, Single pad */		
+  [12] = 0x0406, 
+     
+  /* Seq 5: Erase Sector */
+  /* CMD:  0x20 - Sector Erase, single pad */
+  /* ADDR: 0x18 - 24 bit address, single pad */		
+  [20] = 0x08180420,
+     
+  /* Seq 8: Erase Block */
+  /* CMD:  0x52 - Block Erase, single pad */
+  /* ADDR: 0x18 - 24 bit address, single pad */			
+  [32] = 0x08180452,
+
+  /* Seq 8: Page Program */
+  /* CMD:    0x02 - Page Program, Single pad */
+  /* ADDR:   0x18 - 24bit address, Single pad */
+  /* WRITE:  0x80 - Write 128 bytes at one pass, Single pad */
+  [36] = 0x08180402, [37] = 0x2080,
+
+  /* Seq 11: Chip Erase*/
+  /* CMD:    0x60 - Erase All chip, Single pad */		
+  [44] = 0x460,      
+  },
+
+};
 
 /*  Initialize Flash Programming Functions
  *    Parameter:      adr:  Device Base Address
@@ -52,14 +125,22 @@ quadspi_nor_config_t flashConfig = {.pageSize = 0x400};
 int Init (unsigned long adr, unsigned long clk, unsigned long fnc) {
 
   memset((void *)&flashConfig, 0U, sizeof(quadspi_nor_config_t));
-  serial_nor_config_option_t configOption;
-  configOption.option0.U = CONFIG_OPTION;
-  status_t status = quadspi_nor_get_config(&flashConfig, &configOption);
 
-  if(!status)
-  {
-    status = quadspi_nor_init(&flashConfig);
-  }
+  flashConfig.memConfig = w25q_config; 
+  flashConfig.pageSize = 0x100;              //!< Page size of Serial NOR
+  flashConfig.sectorSize = 0x1000;            //!< Sector size of Serial NOR
+  flashConfig.ipcmdSerialClkFreq = 0;     //!< Clock frequency for IP command
+  flashConfig.isUniformBlockSize = 0;     //!< Sector/Block size is the same
+  flashConfig.isDataOrderSwapped = 0;     //!< Data order (D0, D1, D2, D3) is swapped (D1,D0, D3, D2)
+  flashConfig.resetLogic = 0;             //!< Reset Logic: 0 - No reset, 1 - Reset Pin, 2 - JEDEC HW reset
+  flashConfig.serialNorType = 0;          //!< Serial NOR Flash type: 0/1/2/3
+  flashConfig.needExitNoCmdMode = 0;      //!< Need to exit NoCmd mode before other IP command
+  flashConfig.halfClkForNonReadCmd = 0;   //!< Half the Serial Clock for non-read command: true/false
+  flashConfig.needRestoreNoCmdMode = 0;   //!< Need to Restore NoCmd mode after IP commmand execution
+  flashConfig.blockSize = 0x8000;             //!< Block size
+  flashConfig.isNonBlockingMode = 0;      //!< Non-blocking mode flag
+
+  status_t status = quadspi_nor_init(&flashConfig);
 
   return status;                                  /* Finished without Errors */
 }
@@ -95,10 +176,20 @@ int EraseChip (void) {
  *    Return Value:   0 - OK,  1 - Failed
  */
 
+void delay(void)
+{
+    volatile uint32_t i = 0;
+    for (i = 0; i < 200000000; ++i)
+    {
+        __asm("NOP"); /* delay */
+    }
+}
+
 int EraseSector (unsigned long adr) {
 
   /*Erase Sector*/
   status_t status =  quadspi_nor_erase(&flashConfig, adr, flashConfig.sectorSize);
+  //delay();
   
   return status;
 }
