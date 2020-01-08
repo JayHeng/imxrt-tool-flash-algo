6b29e62e453aa0ff598ad81a74dc9ee20dc28556
Jay Heng <jie.heng@nxp.com>
2020-01-08 19:50:52 +0800

    [RT600 B0][JLink] Update FlashPrg
    - Took use of new implementation of api (in RT600_B0_PORTB_flashloader(iar&keil))
      - Need configBootClk() in Init()
      - FLASH_BASE_ADDR was used in Flash API
    - .JLinkScript should be put in \SEGGER\JLink_V652e\Devices\NXP\iMXRT6xx, or Jlink commander will not work
      Note: the .JLinkScript comes from \iar&segger_patch_for_rt600_b0_1210\JLink_V652\Devices\NXP\iMXRT6xx
    - Remove "--entry 0xc001" in project option, so the entry depends on .Target.lin
    
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- boards/nxp_evkmimxrt685_rev.e/flash_algo_b0_silicon/Keil_JLink/FlashPrg.c
+++ boards/nxp_evkmimxrt685_rev.e/flash_algo_b0_silicon/Keil_JLink/FlashPrg.c
@@ -34,16 +34,73 @@
 #include "FlashOS.H"        // FlashOS Structures
 #include <string.h>
 #include "bl_api.h"
+#include "cmsis_compiler.h"
 
 /** local definitions **/
 
 #define FLASH_BASE_ADDR 0x08000000
 #define FLEXSPI_INSTANCE_0 (0)
 #define FLEXSPI_INSTANCE_SEL FLEXSPI_INSTANCE_0
-#define CONFIG_OPTION 0xc0000000
+#define CONFIG_OPTION0 0xc1503051
+#define CONFIG_OPTION1 0x20000014
 
+#define MEM_WriteU32(addr, value)  (*((volatile uint32_t *)(addr)) = value)
 
 flexspi_nor_config_t flashConfig = {.pageSize = 0x400};
+
+void configBootClk(void)
+{
+  uint32_t v;
+  __disable_irq();
+  // Disable cache
+  MEM_WriteU32(0x40033800, 0);
+  MEM_WriteU32(0x4003301C, 0);
+  // PMC->MEMSEQCTRL = 0x1U;
+  MEM_WriteU32(0x40135030, 0x1U);
+  // Power FFRO
+  MEM_WriteU32(0x40002630, 0x10000U);
+  /* Flexspi SRAM APD/PPD */
+  MEM_WriteU32(0x40002634, 0xCU);
+  // Power SRAM
+  MEM_WriteU32(0x40002638, 0xFFFFFFFF);
+  MEM_WriteU32(0x4000263C, 0xFFFFFFFF);
+
+  // PMC CTRL APPLYCFG
+  *((volatile uint32_t *)0x4013500C) = *((volatile uint32_t *)0x4013500C) | 1;
+  // WAIT PMC update done
+  do {
+      v = *((volatile uint32_t *)0x40135004) & 1;
+  } while (v);
+
+  /* MAINCLKSELA */
+  MEM_WriteU32(0x40001430, 0x3U);
+  /* MAINCLKSELB */
+  MEM_WriteU32(0x40001434, 0x0U);
+
+  flexspi_nor_set_clock_source(kFlexSpiClockSrc_FFRO_Clk);
+  //CLKCTL0->PSCCTL0_SET = CLKCTL0_PSCCTL0_SET_FLEXSPI_OTFAD_CLK_MASK;
+  MEM_WriteU32(0x40001040, 0x10000U);
+  //RSTCTL0->PRSTCTL0_CLR = RSTCTL0_PRSTCTL0_CLR_FLEXSPI_OTFAD_MASK;
+  MEM_WriteU32(0x40000070, 0x10000U);
+
+  //IOPCTL->PIO[2][12] = 0x130;
+  MEM_WriteU32(0x40004130, 0x130U);
+  //CLKCTL1->PSCCTL1_SET = CLKCTL1_PSCCTL1_SET_HSGPIO2_CLK_SET_MASK;
+  MEM_WriteU32(0x40021044, 0x4U);
+  //RSTCTL1->PRSTCTL1_CLR = RSTCTL1_PRSTCTL1_CLR_HSGPIO2_RST_CLR_MASK;
+  MEM_WriteU32(0x40020074, 0x4U);
+  // GPIO->DIR[2] = 1 << 12;
+  MEM_WriteU32(0x40102008, 0x1000U);
+  // GPIO->CLR[2] = 1 << 12;
+  MEM_WriteU32(0x40102288, 0x1000U);
+  // Delay 400us to reset external flash
+  for(uint32_t i =0; i < 6000; i++)
+    __NOP();
+  // GPIO->SET[2] = 1 << 12;
+  MEM_WriteU32(0x40102208, 0x1000U);
+  // Clear FLASH status store register
+  MEM_WriteU32(0x40002380, 0x0U);
+}
 
 /*  Initialize Flash Programming Functions
  *    Parameter:      adr:  Device Base Address
@@ -54,17 +111,13 @@ flexspi_nor_config_t flashConfig = {.pageSize = 0x400};
 
 int Init (unsigned long adr, unsigned long clk, unsigned long fnc) {
 
+  configBootClk();
   memset((void *)&flashConfig, 0U, sizeof(flexspi_nor_config_t));
   serial_nor_config_option_t configOption;
-  configOption.option0.U = CONFIG_OPTION;
-  status_t status = flexspi_nor_get_config(FLEXSPI_INSTANCE_SEL, &flashConfig, &configOption);
+  configOption.option0.U = CONFIG_OPTION0;
+  configOption.option1.U = CONFIG_OPTION1;
 
-  if(!status)
-  {
-    status = flexspi_nor_flash_init(FLEXSPI_INSTANCE_SEL, &flashConfig);
-  }
-
-  return status;                                  /* Finished without Errors */
+  return (int)flexspi_nor_auto_config(FLEXSPI_INSTANCE_SEL, &flashConfig, &configOption);
 }
 
 
@@ -89,7 +142,7 @@ int EraseChip (void) {
   /*Erase all*/
   status_t status =  flexspi_nor_flash_erase_all(FLEXSPI_INSTANCE_SEL, &flashConfig);
   
-  return status;
+  return (int)status;
 }
 
 
@@ -101,9 +154,9 @@ int EraseChip (void) {
 int EraseSector (unsigned long adr) {
 
   /*Erase Sector*/
-  status_t status =  flexspi_nor_flash_erase(FLEXSPI_INSTANCE_SEL, &flashConfig, adr, flashConfig.sectorSize);
+  status_t status =  flexspi_nor_flash_erase(FLEXSPI_INSTANCE_SEL, &flashConfig, adr - FLASH_BASE_ADDR, flashConfig.sectorSize);
   
-  return status;
+  return (int)status;
 }
 
 
@@ -122,8 +175,8 @@ int ProgramPage (unsigned long adr, unsigned long sz, unsigned char *buf) {
                                        buf+=flashConfig.pageSize,
                                        adr+=flashConfig.pageSize)
   {
-    status =  flexspi_nor_flash_page_program(FLEXSPI_INSTANCE_SEL, &flashConfig, adr, (uint32_t *)buf);
+    status =  flexspi_nor_flash_page_program(FLEXSPI_INSTANCE_SEL, &flashConfig, adr - FLASH_BASE_ADDR, (uint32_t *)buf);
   }
 
-  return status;
+  return (int)status;
 }
