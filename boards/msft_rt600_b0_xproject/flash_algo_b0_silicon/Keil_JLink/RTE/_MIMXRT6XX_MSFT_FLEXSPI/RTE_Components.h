fdc3e116ec3e3711878e9eb98db0b94086a1b9c7
Jay Heng <jie.heng@nxp.com>
2020-01-08 20:11:34 +0800

    [RT600 B0][JLink][MSFT] Initial project based on EVK version
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

---
+++ boards/msft_rt600_b0_xproject/flash_algo_b0_silicon/Keil_JLink/RTE/_MIMXRT6XX_MSFT_FLEXSPI/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'MIMXRT6XX_MSFT_FLEXSPI' 
+ * Target:  'MIMXRT6XX_MSFT_FLEXSPI' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM33_DSP_FP_TZ.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
